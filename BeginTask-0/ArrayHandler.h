69ec17974d7883f7d140232493fc10374b34acc5
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2024-10-06 22:45:02 +0300

    432

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -45,30 +45,7 @@ public:
     //     // }
     //     // return false;
     // }
-    bool IsContains(T __restrict* elem) const {
-        const T __restrict* end = _array + _count;
-        const T __restrict* ptr = _array;
-
-
-        while (ptr + 16 <= end) {
-            if (*ptr == elem || *(ptr + 1) == elem || *(ptr + 2) == elem || *(ptr + 3) == elem ||
-                *(ptr + 4) == elem || *(ptr + 5) == elem || *(ptr + 6) == elem || *(ptr + 7) == elem ||
-                *(ptr + 8) == elem || *(ptr + 9) == elem || *(ptr + 10) == elem || *(ptr + 11) == elem ||
-                *(ptr + 12) == elem || *(ptr + 13) == elem || *(ptr + 14) == elem || *(ptr + 15) == elem) {
-                return true;
-            }
-            ptr += 16;
-        }
-
-        while (ptr < end) {
-            if (*ptr == elem) {
-                return true;
-            }
-            ++ptr;
-        }
-
-        return false;
-    }
+    
     
 
     T GetMax() const {
