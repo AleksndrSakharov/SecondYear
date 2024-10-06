4d9b78e8f6fd99190420ae5133f2e6b0c4b39cb8
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2024-10-06 20:19:33 +0300

    321213

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,32 +1,63 @@
+#pragma once
+#include <cstdlib>
+#include <algorithm>
+#include <stdexcept>
 
 template<typename T>
 class ArrayHandler {
 private:
+    T *_array;
     size_t _size;
-    T* _array;
+    size_t _count;
+    T _maxT;
+    T _minT;
+
+    void Resize(size_t new_size) {
+        T *new_array = new T[new_size];
+        std::move(_array, _array + _count, new_array);
+        delete[] _array;
+        _array = new_array;
+        _size = new_size;
+    };
+
 public:
-    ArrayHandler(size_t size = 10) {
+    ArrayHandler() : _size(10), _count(0) {
+        _array = new T[_size];
+    };
 
-    }
+    ArrayHandler(size_t size) : _size(size), _count(0) {
+        _array = new T[_size];
+    };
 
-    void AppendElem(T elem) {
+    void AppendElem(const T &elem) {
+        if (_count == _size) {
+            Resize(_size * 128);
+        }
+        if (_count == 0){
+            _maxT = elem;
+            _minT = elem;
+        }else{
+            if (_maxT < elem) _maxT = elem;
+            if (_minT > elem) _minT = elem;
+        }
+        _array[_count++] = elem;
+    };
 
-    }
+    bool IsContains(const size_t &elem) const {
+        return std::find(_array, _array + _count, elem) != (_array + _count);
+    };
 
-    bool IsContains(T elem) {
-        return false;
-    }
+    T GetMax() const {
+        if (_count == 0) throw std::runtime_error("Array is empty");
+        return _maxT;
+    };
 
-    T GetMax() {
-        return 0;
-    }
-
-    T GetMin() {
-        return 0;
-    }
+    T GetMin() const {
+        if (_count == 0) throw std::runtime_error("Array is empty");
+        return _minT;
+    };
 
     ~ArrayHandler() {
-
-    }
-
+        delete[] _array;
+    };
 };
