c601a7fbf82e381c329cbae97f360674e58468ed
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2024-10-06 22:41:22 +0300

    3443

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -39,13 +39,36 @@ public:
         _array[_count++] = elem;
     }
 
-    bool IsContains(const T &elem) const {
-        // for (size_t i = 0; i < _count; ++i) {
-        //     if (_array[i] == elem) return true;
-        // }
-        // return false;
-    }
+    // bool IsContains(const T &elem) const {
+    //     // for (size_t i = 0; i < _count; ++i) {
+    //     //     if (_array[i] == elem) return true;
+    //     // }
+    //     // return false;
+    // }
+    bool IsContains(T __restrict* elem) const {
+        const T __restrict* end = _array + _count;
+        const T __restrict* ptr = _array;
 
+
+        while (ptr + 16 <= end) {
+            if (*ptr == elem || *(ptr + 1) == elem || *(ptr + 2) == elem || *(ptr + 3) == elem ||
+                *(ptr + 4) == elem || *(ptr + 5) == elem || *(ptr + 6) == elem || *(ptr + 7) == elem ||
+                *(ptr + 8) == elem || *(ptr + 9) == elem || *(ptr + 10) == elem || *(ptr + 11) == elem ||
+                *(ptr + 12) == elem || *(ptr + 13) == elem || *(ptr + 14) == elem || *(ptr + 15) == elem) {
+                return true;
+            }
+            ptr += 16;
+        }
+
+        while (ptr < end) {
+            if (*ptr == elem) {
+                return true;
+            }
+            ++ptr;
+        }
+
+        return false;
+    }
     
 
     T GetMax() const {
