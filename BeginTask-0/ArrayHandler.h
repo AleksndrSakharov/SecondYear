fe388ed47fc8c81f5f975d2022f0ed735c859c6b
Aleksandr_Sakharov <mr.11aleksandrsasharov11@mail.ru>
2024-10-11 13:36:47 +0300

    231312

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -11,8 +11,8 @@ private:
     size_t _count;
     T _maxT;
     T _minT;
- 
-public:
+  
+public: 
     ArrayHandler() : _size(1000000), _count(0), _minT(std::numeric_limits<T>::max()), _maxT(std::numeric_limits<T>::min()) {
         _array = new T[_size];
     }
